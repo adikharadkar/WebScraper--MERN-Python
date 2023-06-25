1579bdee2bba5fcd2d3e92367d6995b81e3f9a07
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-06-26 03:53:24 +0530

    Commit

--- client/src/Components/ProfilePage/ProfilePage.js
+++ client/src/Components/ProfilePage/ProfilePage.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import '../../Styles/ProfilePage/ProfilePage.css';
+
 const ProfilePage = () => {
     return (
-        <div></div>
+        <div>
+            <div className="profile__summary">
+                <h2 className='profile__user-name'>Aditya Kharadkar</h2>
+                <p className='profile__job-summary'>
+                    <p className='profile__job-position'>Frontend Developer at </p>
+                    <p className='profile__job-company'>&nbsp;Tata Consultancy Services,&nbsp;</p>
+                    <p className='profile__job-location'>Pune</p>
+                </p>
+            </div>
+        </div>
     );
 }
 
