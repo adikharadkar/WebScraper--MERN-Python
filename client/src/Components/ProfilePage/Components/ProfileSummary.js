7a99e3d792a08a694474295723cbc9a5d82896df
Aditya Kharadkar <adityakharadkar27@gmail.com>
2023-06-27 15:12:26 +0530

    Added dynamic summary component

--- client/src/Components/ProfilePage/Components/ProfileSummary.js
+++ client/src/Components/ProfilePage/Components/ProfileSummary.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../Styles/ProfilePage/ProfilePage.css';
+import InputField from '../../InputField';
+import Button from '../../Button';
 
 const ProfileSummary = () => {
+    const [clicked, setClicked] = useState(false);
+    const editBtnClickedHandler = () => {
+        setClicked(!clicked);
+    }
     return (
         <div className="profile__summary">
-            <span class="material-symbols-outlined update-btn">edit</span>
-            <h2 className='profile__header'>Aditya Kharadkar</h2>
-            <p className='profile__job-summary'>
-                <h3>Frontend Developer at Tata Consultancy Services, Pune</h3>
-            </p>
+            <h1 className="profile__header">Profile Summary</h1>
+            {
+                !clicked ? 
+                <div>
+                    <span class="material-symbols-outlined update-btn" onClick={editBtnClickedHandler}>edit</span>
+                    <h2 className='profile__header'>Aditya Kharadkar</h2>
+                    <p className='profile__job-summary'>
+                        <h3>Frontend Developer at Tata Consultancy Services, Pune</h3>
+                    </p>
+                </div> : 
+                <div>
+                    <InputField type="text" name="firstName" placeholder="Enter first name" />
+                    <InputField type="text" name="lastName" placeholder="Enter last name" />
+                    <InputField type="text" name="title" placeholder="Enter a title" />
+                    <Button name="summaryBtn" value="Update" width="100px" />
+                    <Button name="cancelSummaryBtn" value="Cancel" width="100px" onClick={editBtnClickedHandler} />
+                </div>
+            }
+            
         </div>
     )
 }
--- server/WebScraper-Server-Node/models/user.js
+++ server/WebScraper-Server-Node/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
     username: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    salt: {type: String, required: true, unique: true}
+    salt: {type: String, required: true, unique: true},
 });
 
 userSchema.plugin(uniqueValidator);
